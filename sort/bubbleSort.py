ed347a2a463c595e9577b25286c728f664391659
harshchill <harshmahto02@gmail.com>
2026-06-01 15:35:56 +0530

    Decreasing order on bubble sort and selection sort

--- sort/bubbleSort.py
+++ sort/bubbleSort.py
@@ -8,6 +8,17 @@ def bubbleSort(a):
 
     print(a)
 
+def deBubbleSort(a):
+    n = len(a)
+
+    for i in range(n):
+        for j in range(n-1,i,-1):
+            if (a[j]>a[j-1]):
+                a[j],a[j-1] = a[j-1],a[j]
+
+    print(a)
+
+
 
 arr = [65,34,78,99,12,43,78,23]
-bubbleSort(arr)
+deBubbleSort(arr)
--- sort/selectionSort.py
+++ sort/selectionSort.py
@@ -9,10 +9,23 @@ def selectionSort(arr):
         arr[i],arr[min]=arr[min],arr[i]
     
 
+def DeSelectionSort(arr):
+    size = len(arr)
+
+    for i in range(size):
+        max = i
+        for j in range(i,size):
+            if(arr[max]<arr[j]):
+                max = j
+        arr[i],arr[max]=arr[max],arr[i]
+    
+
 
     
 a = [54,44,23,74,676,86,34]
 
-selectionSort(a)
+# selectionSort(a)
+# print(a)
+DeSelectionSort(a)
 print(a)
 
